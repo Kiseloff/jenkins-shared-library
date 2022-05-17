42e4f74da6081e5e855dd8192d2f10db9f5e39ff
Kiseloff <30468578+Kiseloff@users.noreply.github.com>
2022-05-17 17:10:39 +0300

    Update buildImage.groovy

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,11 +1,10 @@
 #!/usr/bin/env groovy
 
-def call(PROJECT_PATH) {
+def call(String repName, String imgName, String projectPath) {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'nexus-credentials', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'ls -la'
-        sh "docker build -t 192.168.88.14:8083/java-maven-app:4.0 ${PROJECT_PATH}"
-        sh "echo $PASS | docker login -u $USER --password-stdin 192.168.88.14:8083"
-        sh 'docker push 192.168.88.14:8083/java-maven-app:4.0'
+        sh "docker build -t ${repName}/${imgName} ${projectPath}"
+        sh "echo $PASS | docker login -u $USER --password-stdin ${repName}"
+        sh "docker push ${repName}/${imgName}"
     }
 }
